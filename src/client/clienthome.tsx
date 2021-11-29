fdda16c0c501dca71117bfb82aa4f086668af738
Frosendal <frederik.rosendal@hotmail.com>
2021-11-29 13:18:39 +0100

    ez fix

--- src/client/clienthome.tsx
+++ src/client/clienthome.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import NavBar from '../shared/navbar';
-import { NavBarRoute } from '../trainer/trainerhome';
 import WorkoutProgramList from '../workout-programs/program-list';
 import ProgramDetails from '../workout-programs/program-details';
+import { NavBarRoute } from '../shared/navRouter';
 
 export default function ClientHome() {
 	const clientRoutes: NavBarRoute[] = [
