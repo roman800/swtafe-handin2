26b358c575a3228fb0c06442a0d3ea735b475c36
roman800 <magnus.roman@hotmail.com>
2021-11-29 13:44:03 +0100

    added create program

--- src/trainer/pages/createProgram.tsx
+++ src/trainer/pages/createProgram.tsx
@@ -1,55 +1,86 @@
-import { Button, FormControl, FormGroup, FormHelperText, FormLabel, Input, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material'
-import React, { useState } from 'react'
-import { IUser, IWorkoutProgram } from '../../api/api'
-
+import { Button, FormControl, Container, FormGroup, FormHelperText, FormLabel, Input, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { IWorkoutProgram, WorkoutProgram } from '../../api/api'
+import { useSelector, useDispatch } from "react-redux";
+import { AppState } from '../../state/store';
+import { getClients } from '../../state/user/user-slice';
+import { useAuth } from '../../auth/authProvider';
+import { apiClient } from '../../state/api-clients';
 
 export default function CreateProgram() {
-    const [createTraining, setCreateTraining] = useState<IWorkoutProgram | undefined>(undefined)
-    const [client, setClient] = useState<IUser | undefined>(undefined)
+    const [createTraining, setCreateTraining] = useState<IWorkoutProgram>({ clientId: 0, exercises: [] })
+    const clients = useSelector((state: AppState) => state.userReducer.clients)
+    const dispatch = useDispatch();
+    const context = useAuth();
+
+    useEffect(() => {
+        dispatch(getClients())
+    }, [dispatch])
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (createTraining && context?.user) {
+            try {
+                await apiClient.workoutProgramsPOST(createTraining as WorkoutProgram)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        (e.target as HTMLFormElement).reset()
+        setCreateTraining({ exercises: [], description: "", name: "" })
+
+    };
 
     return (
-        <Stack direction="column" height="1" alignItems="center" justifyContent="space-around" >
+        <Container maxWidth="sm">
+            <Stack onSubmit={handleSubmit} component="form" direction="column" height="1" alignItems="center" justifyContent="space-around"  >
+                <FormControl component="form" sx={{ width: "fit-content" }}>
+                    <FormLabel component="legend">Title</FormLabel>
+                    <FormGroup>
+                        <Input type="text"
+                            value={createTraining.name}
+                            defaultValue=""
+                            onChange={(value) => { setCreateTraining({ ...createTraining, name: value.target.value }) }}
+                        ></Input>
+                    </FormGroup>
+                    <FormHelperText></FormHelperText>
+                </FormControl>
+                <FormControl component="form">
+                    <FormLabel component="legend">Beskrivelse</FormLabel>
+                    <FormGroup>
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Beskrivelse"
+                            value={createTraining.description}
+                            multiline
+                            rows={4}
+                            defaultValue=""
+                            onChange={(value) => { setCreateTraining({ ...createTraining, description: value.target.value }) }}
 
-            <FormControl component="form" sx={{ width: "fit-content" }}>
-                <FormLabel component="legend">Title</FormLabel>
-                <FormGroup>
-                    <Input type="text"
-                        onChange={(value) => { setCreateTraining({ ...createTraining, name: value.target.value }) }}
-                    ></Input>
-                </FormGroup>
-                <FormHelperText></FormHelperText>
-            </FormControl>
-            <FormControl component="form">
-                <FormLabel component="legend">Beskrivelse</FormLabel>
-                <FormGroup>
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Beskrivelse"
-                        multiline
-                        rows={4}
-                        defaultValue=""
-                    />
-                </FormGroup>
-                <FormHelperText></FormHelperText>
-            </FormControl>
-            <FormControl>
+                        />
+                    </FormGroup>
+                    <FormHelperText></FormHelperText>
+                </FormControl>
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
 
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={client?.firstName + " " + client?.lastName}
-                    label="Klient"
-                    onChange={(value) => setCreateTraining({ ...createTraining, clientId: parseInt(value.target.value) })}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
+                    <InputLabel id="select-client">Klient</InputLabel>
+                    <Select
+                        labelId="select-client"
+                        id="demo-simple-select"
+                        value={Number(createTraining.clientId)}
+                        label="Klient"
+                        onChange={(value) => setCreateTraining({ ...createTraining, clientId: Number(value.target.value) })}
+                    >
+
+                        {clients.map(value => < MenuItem key={value.userId} value={value.userId} > {value.firstName} {value.lastName}</ MenuItem >)}
+                    </Select>
+                </FormControl>
                 <Button variant="contained" type="submit">
-                    Login
+                    Create workout
                 </Button>
-            </FormControl>
-        </Stack>
+            </Stack>
+        </Container>
     )
 }
+
+
